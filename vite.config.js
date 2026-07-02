9a10db8b49904c62ac06a761a2455ca9d68f7cbe
Emile De Bruyckere <emiledebruyckere@gmail.com>
2026-07-02 11:15:36 +0200

    Implemented friendly reminder to avoid people touching this file.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
+// Don't you dare touch it
 export default defineConfig({
   plugins: [svelte()],
 })
